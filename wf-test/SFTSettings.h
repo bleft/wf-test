3725cf32381d0e566dd823f684b8cabfd69dfe35
bleft <sbz@slooft.com>
2013-08-14 12:00:54 +0200

    class SFTSettings for UserDefaults

---
+++ wf-test/SFTSettings.h
@@ -0,0 +1,13 @@
+//
+//  SFTSettings.h
+//  wf-test
+//
+//  Created by Sven Bautz on 14.08.13.
+//  Copyright (c) 2013 slooft.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFTSettings : NSObject
+
+@end
---
+++ wf-test/SFTSettings.m
@@ -0,0 +1,17 @@
+//
+//  SFTSettings.m
+//  wf-test
+//
+//  Created by Sven Bautz on 14.08.13.
+//  Copyright (c) 2013 slooft.com. All rights reserved.
+//
+
+#import "SFTSettings.h"
+
+@implementation SFTSettings
+// als property im AppDelegate anlegen.
+// beim Init Werte aus UserDefaults lesen.
+// Jede Einstellung als eigenes property.
+// im setter speichern in UserDefaults erledigen.
+//
+@end
